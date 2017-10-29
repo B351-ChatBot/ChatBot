478db2a42e89704b4b8d08fb17dab07ad1ebee36
Tiana <33183660+93T@users.noreply.github.com>
2017-10-29 16:12:32 -0400

    Update chat_framework.py

--- chat_framework.py
+++ chat_framework.py
@@ -2,12 +2,12 @@ import tkinter as tk
 
 class Chat:
     def __init__(self):
-        self.root = tk.Tk()
-        self.root.title("Chat Bot")
+        self.root = tk.Tk() # helps with framework GUI setup
+        self.root.title("Chat Bot")  # title
         
-        self.entryText = tk.StringVar()
-        self.e = tk.Entry(self.root, textvariable=self.entryText, bd=5)
-        self.e.bind('<Return>', self.user_response(self.e.get()))
+        self.entryText = tk.StringVar()  
+        self.e = tk.Entry(self.root, textvariable=self.entryText, bd=5) # textbox (can't seem to get this to pass the valus, the issue is with this)
+        self.e.bind('<Return>', self.user_response(self.e.get())) # for keyboard (allows users to press enter to send message)
         self.e.pack(side="left")
 
         # Links 17 through 26 are comminted out untill I can get the textbox to take the enter key.
@@ -26,13 +26,18 @@ class Chat:
         '''
         self.root.mainloop()
 
+    # for Database accessing (wasn't sure if we would need this)
     def data_access(self):
         print("")
 
+    # for users reponse
+    # will print the user's message on screen eventually, just prints to shell/commandline for now
     def user_response(self, event):
         print("working")
         print(event.widget.get())
 
+    # for AI response
+    # should randomly pick a work and otuput it. Will use a list untill database is set up.
     def ai_response(self):
         print("")
     
