e47872d3557d7ac1fc7a41bbd1a7d5dc71771c04
Tiana <33183660+93T@users.noreply.github.com>
2017-10-31 01:02:37 -0400

    Update chat_framework.py

--- chat_framework.py
+++ chat_framework.py
@@ -2,42 +2,24 @@ import tkinter as tk
 
 class Chat:
     def __init__(self):
-        self.root = tk.Tk() # helps with framework GUI setup
-        self.root.title("Chat Bot")  # title
-        
-        self.entryText = tk.StringVar()  
-        self.e = tk.Entry(self.root, textvariable=self.entryText, bd=5) # textbox (can't seem to get this to pass the valus, the issue is with this)
-        #self.e.bind('<Return>', self.user_response(self.e.get())) # for keyboard (allows users to press enter to send message)
+        self.root = tk.Tk()
+        self.root.title("Chat Bot")
+
+        self.w = tk.Message(self.root, text="", width=50)
+
+        self.e = tk.Entry(self.root, bd=5, width=50)
+        self.e.bind(sequence='<Return>', func=self.user_response)
         self.e.pack(side="left")
-
-        # Links 17 through 26 are comminted out untill I can get the textbox to take the enter key.
-        # could not get to take this button below either.
-        # Link 17 will be used eventually, for now i have been working on getting it to print the output in the shell I'm useing.
-        
-        #self.w = tk.Message(self.root, ...)
-        
-          
-        self.hi_there = tk.Button(self.root)
-        self.hi_there["text"]="Reply"
-        self.hi_there["command"]=self.user_response()
-        self.hi_there.pack(side="right")
-
-        print("working")
         
         self.root.mainloop()
 
-    # for Database accessing (wasn't sure if we would need this)
     def data_access(self):
-        print("")
+         x = sample(l_words, 1)
+         self.w.configure(text=self.x)
 
-    # for users reponse
-    # will print the user's message on screen eventually, just prints to shell/commandline for now
-    def user_response(self):
-        print("working")
-        print(self.e.get())
+    def user_response(self, event):
+        self.w.configure(text=self.e.get())
 
-    # for AI response
-    # should randomly pick a work and otuput it. Will use a list untill database is set up.
     def ai_response(self):
         print("")
     
