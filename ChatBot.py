60e13b0b6f2cee297d03e0c0b04d2310bc219938
David Bickel <jdbickel@ads.iu.edu>
2017-11-14 20:23:02 -0500

    updated code to pull answers from corpus but still chosing at random..

---
+++ ChatBot.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+"""
+B351 - Project Chatbot
+
+Project Team:
+Tiana Deckard
+Hans Thieme
+David Bickel
+
+"""
+
+import numpy as np
+import tensorflow as tf
+import re
+import random
+import time
+
+class ChatBot:
+    
+    def __init__(self,corpusTXT,corpusMAP):
+        #load cornel corpus aquired from https://github.com/suriyadeepan/practical_seq2seq/blob/master/datasets/cornell_corpus/
+        self.movieLines = open(corpusTXT, encoding='utf-8', errors='ignore').read().split('\n')
+        self.convLines = open(corpusMAP, encoding='utf-8', errors='ignore').read().split('\n')
+        #dictionary to hold ID to line mapping data
+        self.dictId2Line = {}
+        #list of conversations
+        self.listConv = []
+
+        #load data into dictionary dictId2Line
+        lineParts = []
+        for line in self.movieLines:
+            lineParts = line.split(' +++$+++ ')
+            if (len(lineParts) == 5):
+                self.dictId2Line[lineParts[0]] = lineParts[4]
+
+        #load conversation data
+        convParts = []
+        for line in self.convLines[:-1]:
+            convParts = line.split(' +++$+++ ')[-1][1:-1].replace("'","").replace(" ","")
+            self.listConv.append(convParts.split(','))
+
+    def converse(self,question):
+        print (str(question))
+        answer = "lol"
+
+        #insert logic to pick proper response, for now just choose at random
+        num = random.randint(0,50000)
+        convToUse = self.movieLines[num]
+        convParts = convToUse.split(' +++$+++ ')
+        
+        answer = self.dictId2Line[convParts[0]]
+        
+        return answer
+
+    def train(self):
+        return 1
+
+
+#def main():
+#   c = ChatBot()
+
+#    print(str(len(c.movieLines)))
+#    answer = c.converse("what")
+#    print(str(answer)) 
+
+
+#main()
--- chat_framework.py
+++ chat_framework.py
@@ -11,16 +11,18 @@ David Bickel
 
 import tkinter as tk
 import random
-import mysqlHelper as myDB
+#import mysqlHelper as myDB
+from ChatBot import *
 
 class Chat:
     #initialize the list of words and start up the gui
     def __init__(self):
         #populate key words from the database
         #self.l = ["hey", "lie", "cat", "dog", "bird", "bat"]"""
-        db = myDB.mysqlHelper('db.ini')#be sure to add the db.ini file per google doc
-        word_query = """SELECT word FROM test_data""";
-        self.l = db.getQueryResult(word_query)
+        #db = myDB.mysqlHelper('db.ini')#be sure to add the db.ini file per google doc
+        #word_query = """SELECT word FROM test_data""";
+        #self.l = db.getQueryResult(word_query)
+        self.myCB = ChatBot('corpus/movie_lines.txt','corpus/movie_conversations.txt')
         
         self.root = tk.Tk()
         self.root.title("Chat Bot")
@@ -42,15 +44,16 @@ class Chat:
     #capture user response and call ai-response to get an answer?
     def user_response(self, event):
         self.t = self.e.get()
+        self.a = self.myCB.converse(self.t)
         self.w = tk.Label(self.root, text=self.t, width=50)
         self.w.pack(fill="both", expand=True, padx=10, pady=10)
 
-        self.ai_response()
+        self.ai_response(self.a)
 
     #test version only returns a random selected word as a response
-    def ai_response(self):
-        num = random.randint(0,15)
-        self.t = self.l[num]
+    def ai_response(self,a):
+        #num = random.randint(0,15)
+        self.t = self.a
         self.w = tk.Label(self.root, text=self.t, width=50)
         self.w.pack(fill="both", expand=True, padx=10, pady=10)
 
